815b959753c0137a557baddbd9369b7ef0bc4ec2
Yassine Ben Slimane <yuginim@gmail.com>
2017-10-04 18:13:08 +0100

    Started fideling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,37 @@
         <title>Laravel</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="/css/app.css">
+        <style>
+            .badge{
+                padding: 5px;
+                font-size: 120%;
+            }
+        </style>
     </head>
     <body>
-        <div class="container">
+        <div class="container" id="app">
             <div class="row">
+                <div class="col-md-8">
+                    drop here
+                </div>
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Question types :</div>
+                            <div class="panel-body">
+                                <div class="badge ui-draggable">Multiple choice</div>
+                                <div class="badge ui-draggable">Multiple </div>
+                                <div class="badge ui-draggable">Multiple choice</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        2
+                    </div>
+                </div>
             </div>
         </div>
+        <script src="/js/app.js"></script>
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,9 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.scripts([
+    'nodes_modules/jquery-ui-dist/jquery-ui.js'
+], 'public/js/app.js');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
