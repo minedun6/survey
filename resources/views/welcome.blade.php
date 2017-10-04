ea7c54412de6d645fd0f964ed170ba071c96c3ed
Yassine Ben Slimane <yuginim@gmail.com>
2017-10-04 18:28:55 +0100

    More fideling with jQuery ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,5 +39,6 @@
             </div>
         </div>
         <script src="/js/app.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     </body>
 </html>
