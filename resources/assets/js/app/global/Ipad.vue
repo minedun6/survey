4ff9630fddc7646c4101d1ec0210b52905196aa1
Yassine Ben Slimane <yuginim@gmail.com>
2017-10-09 17:56:56 +0100

    Started working on Ipad Grid Component.

---
+++ resources/assets/js/app/global/Ipad.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ipad">
+
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    .ipad{
+        width: 1270px;
+        height: 854px;
+        margin: 1px auto;
+        padding-top: 45px;
+        border: 1px solid #a0a3aa;
+        border-radius: 62px;
+        background: url('/public/images/iPad.png') center no-repeat;
+    }
+</style>
