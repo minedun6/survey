4ccc883a053e921cd6900568cea84b93e9bc652a
Yassine Ben Slimane <yuginim@gmail.com>
2017-10-11 17:23:41 +0100

    Fixed slot definitions.

--- resources/assets/js/app/global/Modal.vue
+++ resources/assets/js/app/global/Modal.vue
@@ -5,20 +5,20 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title">
-                        <slot name="title">
+                        <div slot="title">
 
-                        </slot>
+                        </div>
                     </h4>
                 </div>
                 <div class="modal-body">
-                    <slot name="body">
+                    <div slot="body">
 
-                    </slot>
+                    </div>
                 </div>
                 <div class="modal-footer">
-                    <slot name="footer">
+                    <div slot="footer">
 
-                    </slot>
+                    </div>
                 </div>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
